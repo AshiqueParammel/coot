975c9a36baf9d3e90f1c42ba97208fc3b9021462
ashiqueparammel <ashiqueparammel@558gmail.com>
2023-08-18 09:25:29 +0530

    updated all

--- static/crop/main.js
+++ static/crop/main.js
@@ -53,7 +53,7 @@ input.addEventListener('change', ()=>{
                     alertBox.innerHTML = `<div class="alert alert-success" role="alert">
                                             Successfully saved and cropped the selected image
                                         </div>`
-                                        window.location.href = `http://65.1.145.166/variant/image_view/${imgId}`;
+                                        window.location.href = `/variant/image_list/${imgId}`;
                                            
                 },
                 
