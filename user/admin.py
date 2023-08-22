0f23b4b0280ac4e3da73c69dcdf311466be5d11c
ashiqueparammel <ashiqueparammel@558gmail.com>
2023-08-22 17:26:19 +0530

    update product adding

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import UserOTP
+from .models import CustomUser
 # Register your models here.
 
 admin.site.register(UserOTP)
+admin.site.register(CustomUser)
